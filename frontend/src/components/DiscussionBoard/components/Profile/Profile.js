844d2cc9515525ca1796c33169faa70b213f9ce2
Michal Turcan <michal.turcan.za@gmail.com>
2022-03-15 07:18:20 +0000

    profile.js update

--- frontend/src/components/DiscussionBoard/components/Profile/Profile.js
+++ frontend/src/components/DiscussionBoard/components/Profile/Profile.js
@@ -3,10 +3,7 @@ import { useState} from 'react'
 
 
 const Profile = ({ userProfile }) => {
-    const clean = DOMPurify.sanitize(content)
-    const clean_subject = DOMPurify.sanitize(subject)
-    const [PostlikesCount, setPostLikesCount] = useState(likes.length)
-
+    const clean_bio = DOMPurify.sanitize(userProfile.bio)
     return (
         <>
             <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"></link>
@@ -15,16 +12,13 @@ const Profile = ({ userProfile }) => {
                         <div class="col-md-4">
                             <div class="well well-light-orange mini-profile-widget">
                                 <div class="image-container">
-                                    <img src="https://bootdey.com/img/Content/avatar/avatar6.png" class="avatar img-responsive" alt="avatar"></img>
+                                <img src={userProfile.avatar} class="avatar img-responsive" alt="avatar"></img>
                                 </div>
                                 <div class="details">
-                                    <h4>John Doe</h4>
+                                <h4>{userProfile.handle}</h4>
                                     <hr>
-                                        <div>Works at Bootdey.com</div>
-                                        <div>Attended University of Bootdey.com</div>
-                                        <div>Lives in Miami, Florida</div>
-                                        <div class="mg-top-10">32 Followers | 120 Following | 18 Posts</div>
-                                    
+                                        <div>{clean_bio}</div>
+                                        <div class="mg-top-10">32 Followers | 120 Following | 18 Posts</div>                             
                                     </hr>
                                 </div>
                             </div>
