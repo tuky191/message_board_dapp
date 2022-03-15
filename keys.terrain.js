798f20bb942b01be2dc1c9bac1e7967e944982f9
Michal Turcan <michal.turcan.za@gmail.com>
2022-03-15 23:09:32 +0000

    updating terrain config

--- keys.terrain.js
+++ keys.terrain.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 module.exports = {
   bombay: {
     mnemonic:
-      "stumble category exchange trumpet thing section find call undo celery summer abstract grocery idea piano select pride current spread exhaust ill alarm giraffe device",
+      process.env.MNEMONIC,
   }
 };
